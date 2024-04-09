edb000adb6c31081dd41aad5925d20895dfc0e15
andrew-vs <andrew.vs@comcast.net>
2024-04-09 11:57:15 -0400

    added handler

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -5,24 +5,30 @@ import android.content.Intent
 import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
 
-var timerBinder: TimerService.TimerBinder? = null
 
-
-val serviceConnection = object: ServiceConnection {
-    override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-        timerBinder = service as TimerService.TimerBinder
-
-    }
-
-    override fun onServiceDisconnected(name: ComponentName?) {
-        timerBinder = null
-    }
-
-}
 class MainActivity : AppCompatActivity() {
+
+    var timerBinder: TimerService.TimerBinder? = null
+
+
+    val serviceConnection = object: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            timerBinder = null
+        }
+
+    }
+
+    val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         )
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
-            timerBinder?.start(100)
+            timerBinder?.start(100, handler)
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
